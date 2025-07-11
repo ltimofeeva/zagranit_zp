7c863c036a8e8628f97c01554f25cb0ffd444cbb
ltimofeeva <43849844+ltimofeeva@users.noreply.github.com>
2025-07-11 15:01:56 +0500

    Update StoneDailyReport.jsx

--- src/StoneDailyReport.jsx
+++ src/StoneDailyReport.jsx
@@ -72,7 +72,7 @@ export default function StoneDailyReport() {
 
     // Запрос на n8n webhook для подгрузки задания
     if (value) {
-      const res = await fetch('https://lpaderina.store/webhook-test/daily_task', {
+      const res = await fetch('https://lpaderina.store/webhook/daily_task', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({ sheet: 'Карпов' }),
